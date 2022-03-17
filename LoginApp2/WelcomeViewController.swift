722273813912791f97ddebbeb5a2a4482648838c
moraWoo <pkstratech@yandex.ru>
2022-03-17 13:22:07 +0500

    Initial commit

---
+++ LoginApp2/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp2
+//
+//  Created by Ildar Khabibullin on 17.03.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
